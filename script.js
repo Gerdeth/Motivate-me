d9fd72d28e446bc62aa59732ee97a9033f44c991
Gerdeth <gerdethwickham@gmail.com>
2021-10-14 17:15:17 -0500

    add quotes

---
+++ script.js
@@ -0,0 +1,16 @@
+function generate(){
+    let quotes= {
+        " -Walt Disney":'“All our dreams can come true, if we have the courage to pursue them.”',
+         "– Mark Twain":'“The secret of getting ahead is getting started.”',
+         "– Babe Ruth":'“It’s hard to beat a person who never gives up.”', 
+         "― William W. Purkey": '“You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth.” ',
+         "― Pablo Picasso":'“Everything you can imagine is real.”', "― Eleanor Roosevelt":'“Do one thing every day that scares you.”', 
+         "― Abraham Lincoln":'“Whatever you are, be a good one.”',
+         "– Catherine Pulsifier":'“If we have the attitude that it’s going to be a great day it usually is.”',
+         "– Aristotle":'“We are what we repeatedly do. Excellence, then, is not an act, but a habit.”',
+         "-Vincent Van Gogh":'“Great things are done by a series of small things brought together”'
+    }
+    let authors = Object.keys(quotes);
+    console.log (authors);
+    document.querySelector
+}
